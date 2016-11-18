ee08afcca38b877a0be28cc9807934564df6a458
keseyha <keseyha11@gmail.com>
2016-11-18 16:24:02 +0700

    first commit

---
+++ app/src/main/java/quick_help/group4/gici4/com/quickhelps/Helps.java
@@ -0,0 +1,13 @@
+package quick_help.group4.gici4.com.quickhelps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Helps extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_helps);
+    }
+}
---
+++ app/src/test/java/quick_help/group4/gici4/com/quickhelps/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package quick_help.group4.gici4.com.quickhelps;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
